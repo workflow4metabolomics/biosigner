6dabcd9106ca7458d05b41bdb06e7a4bc06809ec
Pierrick Roger <pk.roger@icloud.com>
2019-05-06 18:10:01 +0200

    Add exit on error.

--- travis_install_deps.sh
+++ travis_install_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies
-R -e "install.packages(c('batch', 'randomForest'), dependencies = TRUE, repos = 'https://cloud.r-project.org/')"
+R -e "install.packages(c('batch', 'randomForest'), dependencies = TRUE, repos = 'https://cloud.r-project.org/')" || exit 1
 
 # Install library
-R -e 'source("http://bioconductor.org/biocLite.R") ; biocLite("biosigner")'
+R -e 'source("http://bioconductor.org/biocLite.R") ; biocLite("biosigner")' || exit 2
