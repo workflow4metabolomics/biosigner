23860e5e561cfbdac96aced0ef9c6a556691fae3
Pierrick Roger <pk.roger@icloud.com>
2019-05-06 17:54:41 +0200

    Simplify deps install.

--- travis_install_deps.sh
+++ travis_install_deps.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# Get all dependencies
-dependencies=$(grep 'requirement.*type="package.*>' biosigner_config.xml | grep -v biodb | sed 's!^.*>r-\([^<>]*\)</.*$!\1!')
-
 # Install dependencies
-for dep in $dependencies ; do
+R -e "install.packages(c('batch', 'randomForest'), dependencies = TRUE, repos = 'https://cloud.r-project.org/')"
 
-	# Get version
-	version=$(grep "requirement.*type=\"package.*>r-$dep" biosigner_config.xml | sed 's/^.*version="\([^"]*\)".*$/\1/')
-
-	# Install package
-	R -e "devtools::install_version('$dep', version = '$version', repos = 'https://cloud.r-project.org/')"
-done
-
+# Install library
 R -e 'source("http://bioconductor.org/biocLite.R") ; biocLite("biosigner")'
